28a78b010def3527ee691836d2c2217c4edca6f0
keefeharris <keefeharrisjr@gmail.com>
2022-09-07 13:57:10 -0400

    Added form to create modal for account information

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Button used to add account to wallet
+const addAccount = document.querySelector('.add-account');
+addAccount.addEventListener('click', function() {
+
+});
+//console.log(addAccount.textContent);
+const wallet = [];
