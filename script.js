f2456aa5cffe20273964d15dee52dd2121f43a7a
keefeharris <keefeharrisjr@gmail.com>
2022-09-12 13:02:03 -0400

    updated user input

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 "use strict";
-function getInputValue() {
-    let cardHolderValue = document.getElementById('card-holder').value;
-    let accountNumber = document.getElementById('account-number').value;
-    let expiryDate = document.getElementById('expiry-date').value;
-    let cardNetwork = document.getElementById('card-network').value;
-    let balance = document.getElementById('balance').value;
-    
-    console.log(cardHolderValue);
-    console.log(accountNumber);
-    console.log(expiryDate);
-    console.log(cardNetwork);
-    console.log(balance);
 
-    // create an object with the rest of inputs
-    // test the inputs and the object
-    // push to array to display card
-}
+const wallet = document.querySelector('.wallet');
+console.log(wallet.textContent);
+/**
+ * Create a div for a card that holds account information.
+ * Add elements in the card that serve as a placeholder for account information.
+ * In script.js, add a function for rendering account information dynamically.
+ * In this function, create a const that contains a html template for the card.
+ * Insert the template in the HTML
+ */
+const button = document.querySelector('.btn');
 
-// close module
+// use forEach loop to clear form when using DRY
+button.addEventListener('click', function handleClick(event) {
+    event.preventDefault();
+
+    const cardHolderInput = document.querySelector('.card-holder');
+    const accountNumberInput = document.querySelector('.account-number');
+    const expiryDateInput = document.querySelector('.expiry-date');
+    const cardNetworkInput = document.querySelector('.card-network');
+    const balanceInput = document.querySelector('.balance');
+
+    const newAccount = {
+        cardHolderInput: cardHolderInput.value,
+        accountNumberInput: accountNumberInput.value,
+        expiryDateInput: expiryDateInput.value,
+        cardNetworkInput: cardNetworkInput.value,
+        balanceInput: balanceInput.value,
+        transactions: [],
+    }
+
+    console.log(newAccount);
+    wallet.push(newAccount);
+    console.log(wallet);
+
+    cardHolderInput.value = '';
+    accountNumberInput.value = '';
+    expiryDateInput.value = '';
+    cardNetworkInput.value = '';
+    balanceInput.value = '';
+})
