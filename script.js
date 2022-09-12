32e56e54a45d0e23d4f1f919f0ed85731cf37cf7
keefeharris <keefeharrisjr@gmail.com>
2022-09-12 18:46:38 -0400

    Setting up functionality

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 "use strict";
 
-const wallet = document.querySelector('.wallet');
-console.log(wallet.textContent);
-/**
- * Create a div for a card that holds account information.
- * Add elements in the card that serve as a placeholder for account information.
- * In script.js, add a function for rendering account information dynamically.
- * In this function, create a const that contains a html template for the card.
- * Insert the template in the HTML
- */
-const button = document.querySelector('.btn');
+const userAccounts = [];
 
-// use forEach loop to clear form when using DRY
-button.addEventListener('click', function handleClick(event) {
+function init() {
+    console.log(userAccounts.length);
+    if (userAccounts.length <= 0) {
+        console.log(`userAccounts array has ${userAccounts.length} objects.`);
+    } else {
+        console.log(`Updated userAccounts array has ${userAccounts.length} objects.`);
+    }
+}
+
+init();
+
+/*
+Page should start out with 'Create your account' header and create account button
+
+if there is no account
+    (create class names for the headers)
+    display 'Create your account' header,
+    display text that say 'Your wallet is empty' animation coming soon
+    display Create Account button to render modal class
+        * Create a div for a card that holds account information.
+        * Add elements in the card that serve as a placeholder for account information.
+        * In script.js, add a function for rendering account information dynamically.
+        * In this function, create a const that contains a html template for the card.
+        * Insert the template in the HTML
+    hide 'Choose your account' header,
+    hide the account-card class 
+    else
+        Display 'Choose your account' header,
+        display Create Account button to render modal class
+        Display account in array that dynamically render new cards
+        hide 'Create your account' header,
+ */            
+
+const container = document.getElementById('container');
+const wallet = document.querySelector('.wallet');
+const createAccountButton = document.querySelector('.create-account');
+const btn = document.querySelector('.btn');
+const addAccountButton = document.querySelector('.add-account');
+
+// Added accounts from clickNewAccount()
+
+
+function clickNewAccount(event) {
     event.preventDefault();
 
     const cardHolderInput = document.querySelector('.card-holder');
@@ -30,13 +62,18 @@ button.addEventListener('click', function handleClick(event) {
         transactions: [],
     }
 
-    console.log(newAccount);
-    wallet.push(newAccount);
-    console.log(wallet);
+    // console.log(newAccount);
+    userAccounts.push(newAccount);
+    // console.log(userAccounts);
 
     cardHolderInput.value = '';
     accountNumberInput.value = '';
     expiryDateInput.value = '';
     cardNetworkInput.value = '';
     balanceInput.value = '';
-})
+
+    init();
+}
+
+// use forEach loop to clear form when using DRY
+addAccountButton.addEventListener('click', clickNewAccount);
